c9cbf6fe680abc75cfb4ba6d9ffb70cc7922827e
kejiahp <popoolakejiah@gmail.com>
2023-04-11 21:49:20 +0100

    updated: links added to news

--- src/Component/News/NewsData.js
+++ src/Component/News/NewsData.js
@@ -137,9 +137,21 @@ export const newsdata = [
     ],
     links: [
       { name: "1) International Cooperation and Advocacy Division", value: "" },
-      { name: "2) Creativity and Innovation Management Unit", value: "" },
-      { name: "3) Environment and Green Manufacturing Unit", value: "" },
-      { name: "4) Membership Satisfaction and Monitoring Unit", value: "" },
+      {
+        name: "2) Creativity and Innovation Management Unit",
+        value:
+          "https://docs.google.com/document/d/1oNJ-WLop4v1e3vOQP6HFxG-KgqaJEv9Z/edit?usp=sharing&ouid=107891646319599945778&rtpof=true&sd=true",
+      },
+      {
+        name: "3) Environment and Green Manufacturing Unit",
+        value:
+          "https://docs.google.com/document/d/10nEZOYpdKMW8WuIh-_pssSPu13dHO93s/edit?usp=sharing&ouid=107891646319599945778&rtpof=true&sd=true ",
+      },
+      {
+        name: "4) Membership Satisfaction and Monitoring Unit",
+        value:
+          "https://docs.google.com/document/d/1zXa7kjG93MO7HNr8Uo1DgpyonvMCuAUB/edit?usp=sharing&ouid=107891646319599945778&rtpof=true&sd=true ",
+      },
       { name: "5) Legal and Compliance Unit", value: "" },
     ],
   },
