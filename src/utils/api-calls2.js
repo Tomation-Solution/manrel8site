559311c3ee1ca1360285e3c64bfcae3e1f8d7ad5
markothedev <markothedevmail@gmail.com>
2023-05-19 16:48:58 +0100

    added FAQ consumption

--- src/utils/api-calls2.js
+++ src/utils/api-calls2.js
@@ -5,3 +5,15 @@ export const whyJoinManApi = async()=>{
     const resp = await publicCmsRequest.get('membership/why-join/public')
     return resp.data.data
 }
+
+export const joinStepsApi = async()=>{
+    const resp = await publicCmsRequest.get('membership/join-step/public')
+    return resp.data.data
+
+}
+
+export const faqApi = async()=>{
+    const resp = await publicCmsRequest.get('membership/faq/public')
+    return resp.data.data
+
+}
