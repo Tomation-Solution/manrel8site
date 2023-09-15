ec9bca2f865fa902649fda870dc36e082cc707a8
markothedev <markothedevmail@gmail.com>
2023-09-15 15:33:16 +0100

    added env to the cms endpoint

--- src/utils/axios-utils.js
+++ src/utils/axios-utils.js
@@ -6,7 +6,7 @@ const TOKEN = user_data?.token;
 
 const BASE_URL = `https://rel8-corporate-backend-production.up.railway.app/tenant/man/`;
 
-const CMS_BASE_URL = `https://web-production-9688.up.railway.app/api`;
+const CMS_BASE_URL = process.env.REACT_APP_CMS_URL|| 'localhost:8000/'
 // const CMS_BASE_URL = `http://localhost:8000/api`;
 
 export const privateRequest = axios.create({
