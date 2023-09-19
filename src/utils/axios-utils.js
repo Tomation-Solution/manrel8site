a45e690a99a5e0cac9e8e92b3bdb8c1194d87c37
markothedev <markothedevmail@gmail.com>
2023-09-19 15:14:01 +0100

    update apis base url

--- src/utils/axios-utils.js
+++ src/utils/axios-utils.js
@@ -4,10 +4,11 @@ export const user_data = JSON.parse(localStorage.getItem("userdata"));
 
 const TOKEN = user_data?.token;
 
-const BASE_URL = `https://rel8-corporate-backend-production.up.railway.app/tenant/man/`;
+const BASE_URL = `https://web-production-81544.up.railway.app/tenant/man/`;
 
 
-const CMS_BASE_URL = process.env.REACT_APP_CMS_URL|| 'localhost:8000/'
+const CMS_BASE_URL = 'http://23.254.238.194:8000/api'
+// const CMS_BASE_URL = process.env.REACT_APP_CMS_URL|| 'localhost:8000/'
 // const CMS_BASE_URL = `http://localhost:8000/api`;
 
 export const privateRequest = axios.create({
