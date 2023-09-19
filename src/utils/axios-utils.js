eb0217bfea32b962a56c935ae6c4bada5a0d76de
markothedev <markothedevmail@gmail.com>
2023-09-19 14:58:43 +0100

    new branch init

--- src/utils/axios-utils.js
+++ src/utils/axios-utils.js
@@ -6,6 +6,7 @@ const TOKEN = user_data?.token;
 
 const BASE_URL = `https://rel8-corporate-backend-production.up.railway.app/tenant/man/`;
 
+
 const CMS_BASE_URL = process.env.REACT_APP_CMS_URL|| 'localhost:8000/'
 // const CMS_BASE_URL = `http://localhost:8000/api`;
 
