ce4c9931db8b13370910b4cef14f5b720933047f
Nwokolo Matthew <markothedevmail@gmail.com>
2023-11-29 20:59:02 +0100

    updated the rel8 base url

--- src/utils/axios-utils.js
+++ src/utils/axios-utils.js
@@ -4,7 +4,7 @@ export const user_data = JSON.parse(localStorage.getItem("userdata"));
 
 const TOKEN = user_data?.token;
 
-const BASE_URL = `https://web-production-81544.up.railway.app/tenant/man/`;
+const BASE_URL = `https://rel8corporate.watchdoglogisticsng.com/tenant/man/`;
 // const BASE_URL = `http://localhost:8001/tenant/man/`;
 
 const CMS_BASE_URL ='https://manwebsitebackend.spnsng.com/api'
