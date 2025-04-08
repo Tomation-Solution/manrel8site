54def6411b0613b708932e5c58d96e67a3ded579
Peter <obidaberechukwu@gmail.com>
2025-04-08 15:51:53 +0100

    updated base_url

--- src/utils/axios-utils.js
+++ src/utils/axios-utils.js
@@ -4,8 +4,8 @@ export const user_data = JSON.parse(localStorage.getItem("userdata"));
 
 const TOKEN = user_data?.token;
 
-const BASE_URL = `https://rel8corporate.watchdoglogisticsng.com/tenant/man/`;
-// const BASE_URL = `http://localhost:8001/tenant/man/`;
+// const BASE_URL = `https://rel8corporate.watchdoglogisticsng.com/tenant/man/`;
+const BASE_URL = `http://localhost:8001/tenant/man/`;
 
 // const CMS_BASE_URL = "https://api.manufacturersnigeria.org/api";
 const CMS_BASE_URL = `http://localhost:8000/api`;
